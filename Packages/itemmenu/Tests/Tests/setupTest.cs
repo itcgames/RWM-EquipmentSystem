c4e43f33e55669e41c478ad65bc026e144cb090b
Ben Stynes <C00239718@itcarlow.ie>
2021-11-07 22:39:31 +0000

    Can not figure out tests

---
+++ Packages/itemmenu/Tests/Tests/setupTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class setupTest
+    {
+        setupItem help;
+         int amount = 99;
+        [Test]
+        public void TestgetAmount()
+        {
+            
+
+            int expected = amount;
+            int output = help.getAmount(); 
+           // CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
+
+
