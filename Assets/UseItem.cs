5d7508ebd4009477262b965047a3ee938e255524
Ben Stynes <C00239718@itcarlow.ie>
2021-11-07 21:16:19 +0000

    Have basic using setup

---
+++ Assets/UseItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UseItem : MonoBehaviour
+{
+    public GameObject Item;
+    public int amount;
+    public string itemName;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private void OnEnable()
+    {
+        Item.GetComponent<setupItem>().changeAmount(1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        amount = Item.GetComponent<setupItem>().getAmount();
+        Item.GetComponentInChildren<Text>().GetComponentInChildren<Text>().text = itemName +" x " + amount.ToString();
+    }
+
+   
+}
